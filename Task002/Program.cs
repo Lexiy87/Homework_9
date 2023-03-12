7043ce674a4772dbf11d2627d27386f49de65b7f
Alexey_Volkov <alexiy87@inbox.ru>
2023-03-12 19:42:15 +0300

    feat: add new files to homework

---
+++ Task002/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов 
+// в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+
+int GetNumber (string message)
+{
+    int number = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out number) )
+        {
+            break;
+        }
+        else System.Console.WriteLine("Вы ввели не верное число повторите ввод");
+    }
+    return number;
+}
+string GetNaturalNumbers(int number1, int number2, int sum)
+{
+    
+    if (number1 == number2+1)
+        {
+            System.Console.WriteLine($"Сумма двуx чисел: {sum}");
+            return " ";
+        }
+    else sum = sum + number1;
+    return GetNaturalNumbers(number1 + 1, number2, sum);
+
+
+}
+
+int FirstNumber = GetNumber("Введите число: ");
+int SecondNumber = GetNumber("Введите число: ");
+int sum = 0;
+GetNaturalNumbers(FirstNumber,SecondNumber, sum);
+
+
+// void SumNumber (int FirstNumber, int SecondNumber)
+// {
+//     int sum = 0;
+//     for (; FirstNumber <= SecondNumber; FirstNumber++)
+//     {
+//         sum = sum + FirstNumber;
+//     }
+//     System.Console.WriteLine(sum);
+    
+// }
+// int FirstNumber = GetNumber("Введите число: ");
+// int SecondNumber = GetNumber("Введите число: ");
+// SumNumber(FirstNumber, SecondNumber); 
---
+++ Task003/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int GetNumber (string message)
+{
+    int number = 0;
+
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out number) && number <=3)
+        {
+            break;
+        }
+        else System.Console.WriteLine("Вы ввели не верное число повторите ввод");
+    }
+    return number;
+}
+int FindAkerman(int m, int n) 
+{
+    if (m == 0)
+    {
+        return n + 1; 
+    }
+    else if ((m != 0) && (n == 0)) 
+    {
+        
+        return FindAkerman(m - 1, 1);
+    }
+    else
+    {
+        return FindAkerman(m - 1, FindAkerman(m, n - 1)); 
+    }
+}
+
+int number1 = GetNumber ("Введите первое число: ");
+int number2 = GetNumber ("Введите второе число: ");
+
+System.Console.WriteLine(FindAkerman(number1,number2));
+
+
